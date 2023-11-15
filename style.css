346853f57862727b04024085a461c2619406e8f3
DDV2412 <dhyanputra24@gmail.com>
2023-11-15 08:59:42 +0700

    Create Theme

---
+++ style.css
@@ -0,0 +1,9 @@
+/* 
+Theme Name: The Minded
+Theme URI: -
+Author: DDV
+Author URI: https://ddv-portfolio.webflow.io/
+Description: A WordPress theme that has been designed with specialist doctor search and booking features takes the user experience to a higher level in undergoing healthcare. With a responsive design and user-friendly interface, this theme brings convenience to users to find, explore and plan appointments with specialist doctors according to their needs.
+Version: 1.0
+Tags: healthcare,medical,doctor,booking,appointment,clinic,hospital,responsive,customizable,professional,schedule,booking-system,patient-management,wordpress-theme,premium-theme,health,appointment-booking,responsive-design,modern,user-friendly
+*/
